e2cbb9e4fba8374c73cf8f10105ddfbc37a8125c
Maxim Golay <maxim.golay@ik.me>
2021-12-23 11:18:21 +0100

    Automatic periodic refresh of dynamic content

--- Static server/src/js/people.js
+++ Static server/src/js/people.js
@@ -1,6 +1,16 @@
 $(function() {
-    $.getJSON("/api/", function(person) {
-            var personDesc = person.firstName + " " + person.lastName + ", " + person.age
-            $("#person").text(personDesc)
-    })
+
+    const TIMEOUT_MS = 1337
+
+    // Retrieves a new JSON payload from the dynamic server
+    function getPerson() {
+        $.getJSON("/api/", function(person) {
+                var personDesc = person.firstName + " " + person.lastName + ", " + person.age
+                $("#person").text(personDesc)
+        })
+    }
+
+    // Gets a first person and refreshes it every so often
+    getPerson()
+    setInterval(getPerson, TIMEOUT_MS)
 })
