ad09efce83cb710e8882b0b3034e050723a94030
Maxim Golay <maxim.golay@ik.me>
2022-01-13 11:26:29 +0100

    Added dynamic content load balancing (RR)

--- dynamic_server/src/index.js
+++ dynamic_server/src/index.js
@@ -2,15 +2,19 @@ var chance = require("chance").Chance()
 var server = require("express")();
 
 const port = 80
+const servnum = chance.natural({min: 1, max: 1000})
 
 server.get('/', (request, response) => {
 
     var names = chance.name().split(' ')
     
     response.send({
-        firstName: names[0],
-        lastName: names[1],
-        age: chance.age()
+        person: {
+            firstName: names[0],
+            lastName: names[1],
+            age: chance.age()
+        },
+        servnum: servnum
     });
 })
 
